30d1c8b4e64b183507352f98f15be8b8ba16b8d4
Michael Bouclas <mbouclas@gmail.com>
2015-08-26 17:03:45 +0300

    various fixes - additions

--- bin/Command/publish.js
+++ bin/Command/publish.js
@@ -2,8 +2,8 @@ module.exports = (function(App){
     var colors = require('colors');
     var fs = require('fs-extra');
     var async = require('async');
-    var baseDir = App.pathName + '/node_modules/';
-    var publicPath = App.pathName + '/public/'
+    var baseDir = App.Config.baseDir + '/node_modules/';
+    var publicPath = App.Config.baseDir + '/public/';
 
     function command(){
         this.name = 'publish';
@@ -22,39 +22,55 @@ module.exports = (function(App){
         this.assetsFolder = '';
 
         for (var a in defaultPaths){
-            if (fs.existsSync(defaultPaths[a])){//Fist default failed
+            if (fs.existsSync(defaultPaths[a])){//First default failed
                 this.assetsFolder = defaultPaths[a];
                 break;
             }
         }
 
-        if (this.assetsFolder.length == 0){
-            callback('assets not found',false);
-        }
-
+        var asyncArr = [];
         var modulePath = publicPath + 'packages/' + this.module;
 
-        var that = this;
-        async.series([
-            checkPublicFolder.bind(null,modulePath),
-            copyFiles.bind(null,this.assetsFolder,modulePath)
-        ],function(err,result){
+        if (this.assetsFolder.length != 0){
+            asyncArr.push(checkFolder.bind(null,modulePath));
+            asyncArr.push(copyFiles.bind(null,this.assetsFolder,modulePath));
+        }
+
+        var that = this,
+            destConfigFolder = App.Config.baseDir + '/App/Config/packages/' + this.module,
+            destLangFolder = App.Config.baseDir + '/App/Lang/packages/' + this.module,
+            moduleConfigFolder = baseFolder + '/Config',
+            langFolder = baseFolder + '/Lang';
+
+        if (fs.existsSync(moduleConfigFolder)){//Check for config files
+            asyncArr.push(checkFolder.bind(null,destConfigFolder));
+            asyncArr.push(publishConfig.bind(null,moduleConfigFolder,destConfigFolder));
+        }
+
+        //publish Lang
+        if (fs.existsSync(langFolder)) {//Check for config files
+            asyncArr.push(checkFolder.bind(null,destLangFolder));
+            asyncArr.push(publishLang.bind(null,langFolder,destLangFolder));
+        }
+
+        async.series(asyncArr,function(err,result){
             console.log(colors.green('command ' + that.name + ' fired'));
             callback(null,true);
         });
-    }
+    };
 
-    function checkPublicFolder(folder,callback){
+    function checkFolder(folder,callback){
 
         fs.mkdirs(folder,function(err){
             if (err){
+                console.log(err,'dir not created');
                 callback(err,'dir not created');
                 return;
             }
 
            callback(null,'Dir created');
         });
-    };
+    }
 
     function copyFiles(src,dest,callback){
         fs.copy(src,dest,function(err){
@@ -66,7 +82,32 @@ module.exports = (function(App){
             callback(null,'files copied');
         });
 
-    };
+    }
+
+    function publishConfig(srcConfigFolder,destConfigFolder,callback){
+        fs.copy(srcConfigFolder,destConfigFolder,function(err){
+            if (err){
+                console.log(err,'files not copied');
+                callback(err,'files not copied');
+                return;
+            }
+
+            callback(null,'files copied');
+        });
+    }
+
+    function publishLang(srcFolder,destFolder,callback){
+        console.log(srcFolder);
+        fs.copy(srcFolder,destFolder,function(err){
+            if (err){
+                console.log(err,'files not copied');
+                callback(err,'files not copied');
+                return;
+            }
+
+            callback(null,'files copied');
+        });
+    }
 
     return command;
 });
--- index.js
+++ index.js
@@ -1,72 +1,20 @@
 module.exports = (function(App){
-    App.serviceProviders = {};
-    App.Database = {};
-    App.templates = {};
-    App.viewDirs = {};
-    var async = require('async');
-    App.lodash = require('lodash');
-    App.async = require('async');
-    var events = require('events');
-    App.Event = new events.EventEmitter();
-    var express = require('express');
-    var app = express();
-    var bodyParser = require('body-parser');
-    var NodeCache = require( "node-cache" );
-    //var baseAppPath = process.env.NODE_PATH.replace(':','').replace(';','');
-    var baseAppPath = App.pathName;
-    var configPath = baseAppPath+'/App/Config';
-
-    App.loadConfig = require('./lib/Framework/Config/loader');
-    App.Config = App.loadConfig(configPath,baseAppPath);
-    App.Cache = new NodeCache();
-    require('./lib/Framework/Log/loader')(App);
-    require('./lib/Framework/Helpers/loader')(App);
-    require('./lib/Framework/Crypt/loader')(App);
-    require('./lib/Framework/Service/loader')(App);
-    var Lang = require('mcms-node-localization');
-    App.EagerLoader = require('mcms-node-eager-loader')();
-    App.Lang = new Lang({
-        locales : App.Config.app.locales
-    });
-
-    /*
-     * Load databases
-     */
-    //default DB
-    App.loadDatabaseDriver = require('./lib/Framework/Database/loader')(App);
-    App.Database[App.Config.database.default] = new App.loadDatabaseDriver(App.Config.database.default);
-
-
-    App.server = app;
-    App.express = express;
-    //App.server.set('view', require('express-prefixed-roots-view'));
-    /*
-    * initialize sessions
-    */
-    require('./lib/Framework/Session/session')(App,bodyParser);
-
-    /*
-     * Auth
-     */
-    require('./lib/Framework/Auth/loader')(App);
-
-    require('./lib/Framework/ServiceProvider/loader')(App.Config.app.serviceProviders,App);
-
-
-    if (typeof App.Config.database.redis != 'undefined' && App.Config.database.redis.host != 'null'){
-        App.Database.redis = require('./lib/Framework/Database/redis')(App.Config.database.redis);
-    }
-
-
-    /*
-     * Load server
-     */
-    App.lift = function(port,callback){
-        port = (typeof port == 'undefined' || port == null) ? App.Config.app.serverPort : port;
-        var server = App.server.listen(port);
-        App.Io = require('./lib/Framework/Socket/loader')(server);
-        callback(App);
-        return server;
+    var Core = {
+        serviceProvider : require('./lib/ServiceProvider/registerServiceProvider')(App),
+        DB : require('./lib/DB/loader')(App),
+        Crypt : require('./lib/Crypt/loader')(App),
+        Helpers : require('./lib/Helpers/loader')(App),
+        Session : require('./lib/Session/session')(App),
+        User : require('./lib/User/user'), //will return a function so that you can load at will
+        Command : require('./lib/Command/loader'),
+        Cache : require('./lib/Cache')(App),
+        Queue : require('./lib/Queue')(App),
+        Mail : require('./lib/Mail')(App),
+        Worker : require('./lib/Queue/taskRunner')(App)
     };
-    return App;
+
+    Core.Helpers.loadHelpers(__dirname + '/lib/Helpers');
+
+    return Core;
+
 });
